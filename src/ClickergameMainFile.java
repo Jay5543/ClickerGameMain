8544bfaa286385a3ceb7b6c896049c1136971864
Jay5543 <Jeremyfloyd95@gmail.com>
2022-01-04 13:21:28 -0500

    lnlkjn

--- src/ClickergameMainFile.java
+++ src/ClickergameMainFile.java
@@ -1,2 +1,48 @@
+import javax.swing.*;
+import java.awt.*;
+//Window
 public class ClickergameMainFile {
+
+
+
+    static JFrame ClickerGameWindow;
+
+
+    public static void main(String[] args) {
+
+        GUI();
+
+    }
+    public static void GUI(){
+
+        ClickerGameWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+    }
+    //counter
+    public DuckCounter(){
+
+    }
+    public static void CounterIncrease(){
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
